3e48026034e4f1b05675e53ba26d6e551a0659a4
Armand Gilles <arm.gilles@gmail.com>
2015-10-02 14:10:49 +0200

    Change one column name for easiest merge in Big file

--- by_commune/DependancePrestaCom.py
+++ by_commune/DependancePrestaCom.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Oct  2 13:47:48 2015
 
-@author: babou
+@author: GILLES Armand
 """
 
 import pandas as pd
@@ -32,14 +32,24 @@ for path_file in files:
         df_temp.columns = year_col
         df = pd.merge(df, df_temp[features_col], how='inner', on='Codes_Insee')
 
+
+# Rename col to have unique name in futur merge
+list_col = []
+for col in df.columns:
+    if "NB_Allocataires" in col:
+        list_col.append(col+"_DPC") # DPC = DependancePrestaCom
+    else:
+        list_col.append(col)
+df.columns = list_col
+
 df.to_csv('../data/by_commune/full_DependancePrestaCom.csv', encoding='utf-8')
 
 ## Features 
-#[u'NB_Allocataires_2009', u'TR50PFRB_2009',
-#u'TR100PFRB_2009', u'NB_Allocataires_2010', u'TR50PFRB_2010',
-#u'TR100PFRB_2010', u'NB_Allocataires_2011', u'TR50PFRB_2011',
-#u'TR100PFRB_2011', u'NB_Allocataires_2012', u'TR50PFRB_2012',
-#u'TR100PFRB_2012', u'NB_Allocataires_2013', u'TR50PFRB_2013',
-#u'TR100PFRB_2013', u'NB_Allocataires_2014', u'TR50PFRB_2014',
-#u'TR100PFRB_2014']
+#[u'NB_Allocataires_2009_DPC',
+#u'TR50PFRB_2009', u'TR100PFRB_2009', u'NB_Allocataires_2010_DPC',
+#u'TR50PFRB_2010', u'TR100PFRB_2010', u'NB_Allocataires_2011_DPC',
+#u'TR50PFRB_2011', u'TR100PFRB_2011', u'NB_Allocataires_2012_DPC',
+#u'TR50PFRB_2012', u'TR100PFRB_2012', u'NB_Allocataires_2013_DPC',
+#u'TR50PFRB_2013', u'TR100PFRB_2013', u'NB_Allocataires_2014_DPC',
+#u'TR50PFRB_2014', u'TR100PFRB_2014']
 
